3c0f5a6fc10aed4d07c47220496f992dd3e4e6a4
ddyudin <d.dyudin@gmail.com>
2017-08-23 11:00:44 +0300

    Sixt Lab

--- Lab1.6/regexp.py
+++ Lab1.6/regexp.py
@@ -16,13 +16,13 @@ def classification(inp_str):
     if s:
         return ('HOST', s.group(1))
     else: #во всех остальных случаях
-        return ('UNCLASSIFIED',)
+        return ('UNCLASSIFIED',) #кортеж из одного элемента
 
 # get file list and open file
 for name in glob.glob(('f:\\Seafile\\p4ne_training\\config_files\\*.txt')):
     with open(name) as f:
         for imp_str in f:
             c = classification(imp_str)
-            if c[0] != 'UNCLASSIFIED':
+            if c[0] != 'UNCLASSIFIED': #removing 'UNCLASSIFIED'
                 print(c)
 
