493120d79d125d5861c02cc8adea961c1b3d6528
ddyudin <Rjntyrf123>
2017-08-30 17:19:58 +0300

    Lab 2.3

--- Lab2.3/apicem.py
+++ Lab2.3/apicem.py
@@ -1,25 +1,30 @@
 import requests, json, pprint
 from flask import *
 
+#web-server
 app = Flask(__name__)
 
+#show list of hosts
 @app.route('/hosts')
 def hosts1():
     return jsonify(hosts())
 
+#show list of devices
 @app.route('/devices')
 def devices1():
     return jsonify(devices())
 
+#show topology
 @app.route('/api/topology')
 def topology1():
     return jsonify(topology())
 
+#show net
 @app.route("/")
 def index():
     return render_template("topology.html")
 
-
+#recieve new ticket
 def new_ticket():
     url = 'https://sandboxapic.cisco.com/api/v1/ticket'
     payload = {"username": "devnetuser", "password": "Cisco123!"}
@@ -28,6 +33,7 @@ def new_ticket():
     #pprint.pprint(json.dumps(response.json()))
     return response.json()['response']['serviceTicket']
 
+#func get hosts
 def hosts():
     controller = 'devnetapi.cisco.com/sandbox/apic_em'
     url = 'https://' + controller + '/api/v1/host'
@@ -35,6 +41,7 @@ def hosts():
     response = requests.get(url, headers=header, verify=False)
     return response.json() ['response']
 
+#func get devices
 def devices():
     controller = 'devnetapi.cisco.com/sandbox/apic_em'
     url = 'https://'+ controller + '/api/v1/network-device'
@@ -42,6 +49,7 @@ def devices():
     response = requests.get(url, headers=header, verify=False)
     return response.json() ['response']
 
+#func get topology
 def topology():
     controller = 'devnetapi.cisco.com/sandbox/apic_em'
     url = 'https://' + controller + '/api/v1/topology/physical-topology'
@@ -49,6 +57,9 @@ def topology():
     response = requests.get(url, headers=header, verify=False)
     return response.json() ['response']
 
+
 if __name__ == '__main__':
+#get new ticket from cisco
     ticket = new_ticket()
+#run web-server
     app.run(debug=True)
