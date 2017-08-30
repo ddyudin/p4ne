839cb36d65c50bb18c23d2be80b1f9a06b772941
ddyudin <Rjntyrf123>
2017-08-30 15:10:01 +0300

    DZ

--- DZ1/addrplan.py
+++ DZ1/addrplan.py
@@ -1,57 +1,40 @@
 import glob
 import re
 import ipaddress
-
-#from openpyxl import #найти функции создания книги, листа, записи на лист
+from openpyxl import * #найти функции создания книги, листа, записи на лист
 
 #function to find ip
-def classification(inp_str):
-    # для строк вида ip address 192.168.1.1 255.255.255.0
-    s = re.match('^ ip address ([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+) ([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)', inp_str)
-    if  s:
-        return (s.group(1), s.group(2))
-    else:
-        return False
 
-#empty dic
-list_of_address = {}
-#list_of_networks =[]
-# get file list and open file
-for name in glob.glob(('f:\\Seafile\\p4ne_training\\config_files\\*.txt')):
-    with open(name) as f:
+list_of_address = set()
+for file_name in glob.glob(('f:\\Seafile\\p4ne_training\\config_files\\*.txt')):
+    with open(file_name) as f:
         for imp_str in f:
-            netaddr = classification(imp_str)
-            if netaddr is not False:
-#                list_of_address [netaddr[0]] = netaddr[1]
-#                list_of_networks = netaddr[0] + '/' + netaddr[1]
-#                list_of_networks = (ipaddress.ip_network(netaddr[0] + '/' + netaddr[1], strict = False))
-#                list_of_networks.append(ipaddress.ip_network(netaddr[0] + '/' + netaddr[1], strict = False))
-                list_of_address = {netaddr[0]:{'mask':netaddr[1]}, 'net': ipaddress.ip_network(netaddr[0] + '/' + netaddr[1], strict = False)}
-                print(list_of_address.keys(), list_of_address.values())
-exit(1)
-list_to_find = list_of_address
-print ('NETWORK\t\t\tMASK\t\t\tGATEWAY')
-#exit(1)
+            s = re.match('^ ip address ([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+) ([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)', imp_str)
+            if s:
+                address = (s.group(1), s.group(2))
+                list_of_address.add(address)
 
-for addr in list_of_address.keys():
-    for to_find in list_to_find.keys():
-        if ipaddress.ip_address(to_find) in ipaddress.ip_network(addr +'/'+list_of_address[addr], strict = False):
-          #  print(ipaddress.ip_network(addr +'/'+list_of_address[addr], strict = False))
-            print('%s\t\t\t%s\t\t\t%s' %ipaddress.ip_network(addr +'/'+list_of_address[addr], strict = False), list_of_address[to_find], to_find)
-    #print('%s\t\t\t%s\t\t\t%s' %addr, list_of_address[addr], addr)
+list_of_networks = set()
+for address in list_of_address:
+    list_of_networks.add(ipaddress.ip_network(address[0] + '/' + address[1], strict = False))
+ln2 = sorted(list_of_networks)
 
+wb = Workbook()
+ws = wb.active
 
+ws['A1'] = 'Network'
+ws['B1'] = 'Mask'
+ws['C1'] = 'Gateway'
+current_row = 2
 
-#    network = ipaddress.ip_network(addr + '/' + list_of_address[addr], strict=False))
-    #print(network, list_of_networks [0])
-#    for y in list_of_networks:
-#        if list_of_networks == network:
-#            print(network, list_of_address[addr], addr)
- #       else:
- #           print(addr, list_of_address[addr])
-#    list_of_networks = ipaddress.ip_network(list_of_address.(), strict = False)
+for network in ln2:
+    for address in list_of_address:
+        if ipaddress.IPv4Interface(address[0] + '/' + address[1]) in network:
+            #print('%s\t\t\t%s\t\t%s' % str(network),str(address[1]), str(address[0]))
+            ws.cell(row=current_row, column=1, value=str(network))
+            ws.cell(row=current_row, column=2, value=address[1])
+            ws.cell(row=current_row, column=3, value=address[0])
+            current_row += 1
+            continue
 
-#for y in list_of_networks:
-#    print (list_of_networks)
-
-#print(sorted(list(set(list_of_address))))
+wb.save('addrplan.xlsx')
